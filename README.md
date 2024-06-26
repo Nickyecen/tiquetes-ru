55209a80fc68ad91b6bcec063e56af6e0fd33a10
Nickyecen <nicolasecendron@gmail.com>
2024-06-26 16:23:22 -0300

    Added menu usage and options

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
 # Tiquetes RU
 Programa destinado a buscar os tíquetes do RU da UFRGS de forma rápida por meio de um script em python.
-## Modo de funcionamento
-O programa vai ler o arquivo .credentials na mesma pasta em que ele está, que deve conter seu cartão UFRGS com os zeros na frente na primeira linha e sua senha na segunda. Ele utiliza essas credenciais para acessar sua conta e imprime todos os tíquetes disponíveis para uso em sua conta.
+
+## Como utilizar
+O programa vai ler o arquivo .credentials na mesma pasta em que ele está, que deve conter seu cartão UFRGS com os zeros na frente na primeira linha e sua senha na segunda. Ele utiliza essas credenciais para acessar sua conta e pegar os 20 primeiros tíquetes disponíveis e usados.
 
 ### Exemplo de arquivo .credentials:
 ```
-00999999
-senha muito difícil de acertar _123
+00579107
+BRT_dm 092004
 ```
+
+## Funcionamento
+O programa vai carregar as informações de tíquetes e te apresentar com diferentes opções de acesso para as informações de seus tíquetes. Mais de uma opção pode ser utilizada por execução, desde que o caracter da opção esteja na chamada, ela aparecerá.
+
+### t (padrão): Pegar um tíquete disponível
+O programa exibe apenas o tíquete mais recente. Essa é a opção padrão, ou seja, caso nada seja informado no input, essa opção será executada.
+
+### d: Ver últimos 20 tíquetes disponíveis
+O programa exibe os últimos 20 tíquetes disponíveis para uso. Caso haja apenas *n* tíquetes disponíveis tal que *n < 20*, imprimirá apenas esses últimos n tíquetes.
+
+### u: Ver últimos 20 tíquetes usados
+O programa exibe os últimos 20 tíquetes utilizados. Caso haja apenas *n* tíquetes utilizados tal que *n < 20*, imprimirá apenas esses últimos n tíquetes.
+
+### D: Ver informação completa dos últimos 20 tíquetes disponíveis
+Mesmo que *d*, mas também exibe valor, data de liberação e vínculo utilizado para cada tíquete.
+
+### U: Ver informação completa dos últimos 20 tíquetes usados
+Mesmo que *u*, mas também exibe valor, tipo, data de liberação, vínculo utilizado, data de consumo e restaurante utilizado para cada tíquete.
+
--- tickets.py
+++ tickets.py
@@ -2,6 +2,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+# Gets user credentials
 cred = open('.credentials', 'r')
 
 USER = cred.readline()
@@ -9,36 +10,44 @@ PSWD = cred.readline()
 
 cred.close()
 
+# fetch_tickets: String String -> Response
+# Obj.: Given the user and password for the connection, gets
+# the ticket information and returns it
 def fetch_tickets(user, pswd):
+    # urls
     portal_url = 'https://www1.ufrgs.br/sistemas/portal/'
     ticket_url = 'https://www1.ufrgs.br/RU/tru/'
 
+    # Session
     session = requests.Session()
 
+    # Logs in to UFRGS
     login_info = {
             'usuario': user,
             'senha': pswd
             }
     portal_response = session.post(portal_url, data=login_info)
-    print('Post portal response:')
-    print(portal_response)
 
+    # Gets tickets
     if portal_response.status_code == 200:
         ticket_response = session.get(ticket_url)
-        print('Ticket response:')
-        print(ticket_response)
         return ticket_response
 
     return None
 
+# filter_table: Tag -> List
+# Obj.: Given a table of tickets, filters it to become a list
+# of dictionary tickets
 def filter_table(table):
     filtered = []
     
+    # Gets the property of each column 
     rows = table.find_all('tr')
     head = rows.pop(0)
     infos = [info.text.strip() for info in head]
     infos.pop(0)    
 
+    # Transforms each ticket row in a dictionary
     for row in rows:
         ticket = {}
         columns = row.find_all('td')
@@ -49,6 +58,7 @@ def filter_table(table):
 
     return filtered
 
+# get_tickets: String -> Dict
 def get_tickets(html):
     soup = BeautifulSoup(html, 'html.parser')
     tables = soup.find_all('table')
@@ -59,8 +69,46 @@ def get_tickets(html):
             }
     return tickets
 
+############ EXECUTION ############
+print('Acessando informações de tíquetes, aguarde...')
 ticket_html = fetch_tickets(USER, PSWD).text
 all_tickets = get_tickets(ticket_html)
+print()
 
-for ticket in all_tickets['available']:
-    print(ticket['Tíquete'])
+# Prompt
+print('O que deseja fazer?')
+print('t (padrão): Pegar um tíquete disponível')
+print('d: Ver últimos 20 tíquetes disponíveis')
+print('u: Ver últimos 20 tíquetes usados')
+print('D: Ver informação completa dos últimos 20 tíquetes disponíveis')
+print('U: Ver informação completa dos últimos 20 tíquetes usados')
+
+# Input
+info = input()
+print()
+
+if info == '' or 't' in info:
+    print('Tíquete disponível:')
+    print(all_tickets['available'][0]['Tíquete'])
+    print()
+if 'd' in info:
+    print('Últimos 20 tíquetes disponíveis:')
+    for index, ticket in enumerate(all_tickets['available']):
+        print(f' {index+1}:\t{ticket['Tíquete']}')
+    print()
+if 'u' in info:
+    print('Últimos 20 tíquetes usados:')
+    for index, ticket in enumerate(all_tickets['used']):
+        print(f' {index+1}:\t{ticket['Tíquete']}')
+    print()
+if 'D' in info:
+    print('Informação completa sobre últimos 20 tíquetes disponíveis:')
+    for ticket in all_tickets['available']:
+        print(str(ticket).replace('{', '').replace('\'', '').replace('}',''))
+    print()
+if 'U' in info:
+    print('Informação completa sobre últimos 20 tíquetes usados:')
+    for ticket in all_tickets['used']:
+        print(str(ticket).replace('{', '').replace('\'', '').replace('}',''))
+    print()
+
